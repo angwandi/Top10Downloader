a9e0a7876ed78712015db8253b7e9e2fcd698432
Innocent <31923567+angwandi@users.noreply.github.com>
2018-09-06 00:46:10 +0100

    Add a ViewHolder class

--- app/src/main/java/app/a2ms/top10downloader/FeedAdapter.kt
+++ app/src/main/java/app/a2ms/top10downloader/FeedAdapter.kt
@@ -8,27 +8,38 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
 
+//ViewHolder class holding references for fields
+class ViewHolder(v: View) {
+    val tvName: TextView = v.findViewById(R.id.tvName)
+    val tvArtist: TextView = v.findViewById(R.id.tvArtist)
+    val tvSummary: TextView = v.findViewById(R.id.tvSummary)
+}
+
+
 class FeedAdapter(context: Context, private val resource: Int, private val applications: List<FeedEntry>) : ArrayAdapter<FeedEntry>(context, resource) {
     private val mTAG = "FeedAdapter"
     private val inflater = LayoutInflater.from(context)
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         Log.d(mTAG, "getView() called")
 
-        val view: View = if (convertView == null) {
+        val view: View
+        val viewHolder: ViewHolder
+        if (convertView == null) {
             Log.d(mTAG, "getView called with null convertView")
-            inflater.inflate(resource, parent, false)
+            view = inflater.inflate(resource, parent, false)
+            viewHolder = ViewHolder(view)
+            view.tag = viewHolder
         } else {
             Log.d(mTAG, "getView provided a convertView")
-            convertView
+            view = convertView
+            viewHolder = view.tag as ViewHolder
         }
-        val tvName: TextView = view.findViewById(R.id.tvName)
-        val tvArtist: TextView = view.findViewById(R.id.tvArtist)
-        val tvSummary: TextView = view.findViewById(R.id.tvSummary)
 
         val currentApp = applications[position]
-        tvName.text = currentApp.name
-        tvArtist.text = currentApp.artist
-        tvSummary.text = currentApp.summary
+
+        viewHolder.tvName.text = currentApp.name
+        viewHolder.tvArtist.text = currentApp.artist
+        viewHolder.tvSummary.text = currentApp.summary
         return view
     }
 
