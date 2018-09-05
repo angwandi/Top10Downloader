2259348fb1f4f75bf3ee8d09d39174bc85b4edd8
Innocent <31923567+angwandi@users.noreply.github.com>
2018-09-05 22:23:48 +0100

    Add ListView and TextView

--- app/src/main/java/app/a2ms/top10downloader/ParseApplication.kt
+++ app/src/main/java/app/a2ms/top10downloader/ParseApplication.kt
@@ -22,7 +22,7 @@ class ParseApplication {
             var eventType = xpp.eventType
             var currentRecord = FeedEntry()
             while (eventType != XmlPullParser.END_DOCUMENT) {
-                val tagName = xpp.name.toLowerCase() //todo: we should use the safe-call operator
+                val tagName = xpp.name?.toLowerCase()
                 when (eventType) {
                     XmlPullParser.START_TAG -> {
                         Log.d(mTAG, "parse: Starting tag for " + tagName)
